b827972455754feb401f7c734ae0770c1a1120e8
Kaykobad Ahammed Yiasin <90813580+kaykobadyiasin@users.noreply.github.com>
2021-12-18 19:38:52 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// window alert
+function winDow(){
+  // window.alert('Please make good use of this web page');
+}
+
+// textChange
+function textChange(){
+
+  document.getElementById('pera').innerHTML = "My country name is Bangladesh";
+}
+function textTwo(){
+  document.getElementById('pera').innerHTML = "16 december is Independent day";
+}
+// textChange
+
+// image change
+function imgOne(){
+  document.getElementById('img1').src = "images/work_1.jpg";
+}
+function imgTwo(){
+  document.getElementById('img1').src = "images/work_2.jpg";
+}
+// image change
+
+// alert
+function alrt(){
+  alert('Please drag bangladesh Flag')
+}
+// alert
+
+// consol log
+function consolLog(){
+  console.log('Bangladesh')
+}
+// contsol log
+
+// var text
+function MultiChange(){
+
+  document.getElementsByClassName('vt')[1].innerHTML = 'Bangladesh';
+}
+function noMultiChange(){
+
+  document.getElementsByClassName('vt')[1].innerHTML = 'My country name is Bangladesh';
+}
+// var text
+
+
+
